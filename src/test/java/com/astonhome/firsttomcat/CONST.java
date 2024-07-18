55cb68763d531fd9af9c07b774a0f7ad3800a043
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-07-18 15:42:57 +0300

    CoachMapperTest

---
+++ src/test/java/com/astonhome/firsttomcat/CONST.java
@@ -0,0 +1,53 @@
+package com.astonhome.firsttomcat;
+
+import com.astonhome.firsttomcat.dto.CoachDTO;
+import com.astonhome.firsttomcat.dto.UserDTO;
+import com.astonhome.firsttomcat.entity.Coach;
+import com.astonhome.firsttomcat.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CONST {
+
+    public static List<UserDTO> USERSOFCOACH1DTO = new ArrayList<>();
+    public static CoachDTO COACH1DTO = new CoachDTO(1L, "coachName1", USERSOFCOACH1DTO);
+
+    public static List<UserDTO> USERSOFCOACH2DTO = null;
+    public static CoachDTO COACH2DTO = new CoachDTO(2L, "coachName2", USERSOFCOACH2DTO);
+
+    public static List<CoachDTO> COACHOFUSER1DTO = new ArrayList<>();
+    public static UserDTO USER1DTO = new UserDTO(1L, "userName1", COACHOFUSER1DTO);
+
+    public static List<CoachDTO> COACHOFUSER2DTO = null;
+    public static UserDTO USER2DTO = new UserDTO(2L, "coachName2", COACHOFUSER2DTO);
+
+    public static List<CoachDTO> COACHOFUSER3DTO = new ArrayList<>(List.of(COACH1DTO, COACH2DTO));
+    public static UserDTO USER3DTO = new UserDTO(3L, "coachName3", COACHOFUSER3DTO);
+
+    public static List<UserDTO> USERSOFCOACH3DTO = new ArrayList<>(List.of(USER1DTO, USER2DTO, USER3DTO));
+    public static CoachDTO COACH3DTO = new CoachDTO(3L, "coachName3", USERSOFCOACH3DTO);
+
+
+
+
+    public static List<User> USERSOFCOACH1 = new ArrayList<>();
+    public static Coach COACH1 = new Coach(1L, "coachName1", USERSOFCOACH1);
+
+    public static List<User> USERSOFCOACH2 = null;
+    public static Coach COACH2 = new Coach(2L, "coachName2", USERSOFCOACH2);
+
+    public static List<Coach> COACHOFUSER1 = new ArrayList<>();
+    public static User USER1 = new User(1L, "userName1", COACHOFUSER1, null);
+
+    public static List<Coach> COACHOFUSER2 = null;
+    public static User USER2 = new User(2L, "coachName2", COACHOFUSER2,"Здоров");
+
+    public static List<Coach> COACHOFUSER3 = new ArrayList<>(List.of(COACH1, COACH2));
+    public static User USER3 = new User(3L, "coachName3", COACHOFUSER3,  "Болен");
+
+    public static List<User> USERSOFCOACH3 = new ArrayList<>(List.of(USER1, USER2, USER3));
+    public static Coach COACH3 = new Coach(3L, "coachName3", USERSOFCOACH3);
+
+
+}
--- src/test/java/com/astonhome/firsttomcat/mapper/CoachMapperTest.java
+++ src/test/java/com/astonhome/firsttomcat/mapper/CoachMapperTest.java
@@ -1,15 +1,46 @@
 package com.astonhome.firsttomcat.mapper;
 
-import org.junit.jupiter.api.Test;
+import com.astonhome.firsttomcat.dto.CoachDTO;
+import com.astonhome.firsttomcat.entity.Coach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static com.astonhome.firsttomcat.CONST.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class CoachMapperTest {
 
-    @Test
-    void toDTO() {
+    private CoachMapper coachMapper;
+
+
+    public static Stream<Arguments> provideParamsForTests() {
+        return Stream.of(
+                Arguments.of(COACH1DTO, COACH1),
+                Arguments.of(COACH2DTO, COACH2),
+                Arguments.of(COACH3DTO, COACH3)
+        );
     }
 
-    @Test
-    void toEntity() {
+    @BeforeEach
+    public void setUp() {
+        coachMapper = new CoachMapperImpl();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTests")
+    void toDTO(CoachDTO outputCoachDTO, Coach inputCoach) {
+        assertEquals(coachMapper.toDTO(inputCoach).getId(), outputCoachDTO.getId());
+        assertEquals(coachMapper.toDTO(inputCoach).getName(), outputCoachDTO.getName());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTests")
+    void toEntity(CoachDTO inputCoachDTO, Coach outputCoach) {
+        assertEquals(coachMapper.toEntity(inputCoachDTO).getId(), outputCoach.getId());
+        assertEquals(coachMapper.toEntity(inputCoachDTO).getName(), outputCoach.getName());
     }
 }
