56f56d00cd6127f6b1f1e90fc6eff73b6efe06ad
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-07-18 13:11:00 +0300

    start doing tests

---
+++ src/test/java/com/astonhome/firsttomcat/mapper/CoachMapperTest.java
@@ -0,0 +1,15 @@
+package com.astonhome.firsttomcat.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CoachMapperTest {
+
+    @Test
+    void toDTO() {
+    }
+
+    @Test
+    void toEntity() {
+    }
+}
